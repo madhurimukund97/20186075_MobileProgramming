07535f3c6428b2aa0b0e601250b1852987880df1
madhurimukund97 <madhurimukund97@msitprogram.net>
2019-03-14 10:35:50 +0530

    act20

--- module2/S03.01-Exercise-RecyclerView/app/src/main/java/com/example/android/sunshine/ForecastAdapter.java
+++ module2/S03.01-Exercise-RecyclerView/app/src/main/java/com/example/android/sunshine/ForecastAdapter.java
@@ -1,4 +1,26 @@
 package com.example.android.sunshine;
 
-class ForecastAdapter {
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+class ForecastAdapter extends RecyclerView.Adapter {
+    public void setWeatherData(String[] weatherData) {
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 }
